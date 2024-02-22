6e1f66b2eb29f8b31e2ede6dd7b3abd177cfed10
Jose Daniel Mata <jdmata16@gmail.com>
2024-02-22 17:32:46 -0600

    Se agrego Iniciar sesión, registrarse y se modifico el índex

---
+++ Proyecto-MVC/Proyecto-MVC/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
